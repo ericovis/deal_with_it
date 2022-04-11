c25f3d8819bcf1eaadc4fdd654dca0199ee520fb
Eric Magalhães <eric@emagalha.es>
2022-04-11 13:45:09 -0300

    converting WORKERS env var to integer

--- gunicorn.py
+++ gunicorn.py
@@ -1,7 +1,7 @@
 import os
 
 PORT = os.environ.get('PORT', 5000)
-WORKERS = os.environ.get('WORKERS', 2)
+WORKERS = int(os.environ.get('WORKERS', 2))
 
 bind = f"0.0.0.0:{PORT}"
 wsgi_app = 'app:app'
